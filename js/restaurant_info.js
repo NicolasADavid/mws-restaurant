932c7706feeb5edd042e90afe5ed355997cd7506
Nicolas David <nicoadav@gmail.com>
2018-09-23 20:43:00 -0400

    mapbox key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidGVycmlmaWNuaWNvIiwiYSI6ImNqbTl4b2hpODU2eXEzcHA0a21ndjZ3b2cifQ.Pl5wNPr4rbcOlt98awMCBA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
